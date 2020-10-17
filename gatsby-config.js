f7f640cfed82c1b079c325a5e2b10d4537ef8768
izaacAbrams <izaac.abrams25@gmail.com>
2020-10-17 15:50:30 -0500

    added articles and paths to articles, added gatsby plugins

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+const siteMetadata = {
+  title: `The New Blog`,
+  description: "This is a tutorial blog learning Gatsby",
+};
+
+module.exports = {
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { graphql } from "gatsby";
+import { Layout } from "../components/Layout";
+import Dump from "../components/Dump";
+
+export default ({ data }) => {
+  return (
+    <>
+      <Layout>
+        <Dump data={data} />
+        {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+          <>
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.date}</p>
+            <p>{excerpt}</p>
+          </>
+        ))}
+      </Layout>
+    </>
+  );
+};
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
